ca80d66645c594a60f7e980cd6d23b933be9f3f2
Jacob Hands <g@gstx.co>
2015-12-18 15:23:03 -0700

    Update command flags

--- cmd/server.go
+++ cmd/server.go
@@ -2,17 +2,41 @@ package cmd
 
 import "github.com/codegangsta/cli"
 
+const (
+	// Flag names
+	flagFolder = "folder"
+	flagSSLPub = "ssl.pub"
+	flagSSLKey = "ssl.key"
+)
+
 // CmdServer will serve the website
 var CmdServer = cli.Command{
 	Name:        "server",
 	ShortName:   "s",
 	Description: "Serves up websites",
 	Action:      runServer,
-	Flags: []cli.Flag{
-		stringFlag("folder", "./sites", "Folder containing websites. Eg. foo.com, bar.com"),
+	Flags:       cmdServerFlags,
+}
+
+// Flags
+var cmdServerFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  flagFolder,
+		Value: "./sites",
+		Usage: "Folder containing websites. Eg. foo.com and bar.com",
+	},
+	cli.StringFlag{
+		Name:  flagSSLPub,
+		Value: "",
+		Usage: "SSL public key for server",
+	},
+	cli.StringFlag{
+		Name:  flagSSLKey,
+		Value: "",
+		Usage: "SSL private key for server",
 	},
 }
 
 func runServer(c *cli.Context) {
-	println("Serving files!", c.String("folder"))
+	println("Serving files!", c.String(flagFolder), c.IsSet(flagFolder))
 }
