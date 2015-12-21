341555d0be2f185e0464a622cf7a8f908f7c516c
Jacob Hands <g@gstx.co>
2015-12-21 11:09:42 -0600

    Add mockobjects file

---
+++ fileserver/mockobjects_test.go
@@ -0,0 +1,29 @@
+package fileserver
+
+import "github.com/stretchr/testify/mock"
+
+// START mockCacheServ
+
+type mockCacheServ struct {
+	mock.Mock
+}
+
+func (m *mockCacheServ) Get(path string) ([]byte, error) {
+	args := m.Called(path)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+// END mockCacheServ
+
+// START mockFileServ
+
+type mockFileServ struct {
+	mock.Mock
+}
+
+func (m *mockFileServ) Get(path string) ([]byte, error) {
+	args := m.Called(path)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+// END mockFileServ
